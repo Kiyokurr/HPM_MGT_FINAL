f4349ababf7dde7e359bb792bfbcf6bbf5c60f65
qw229 <qi.wang.qw229@yale.edu>
2023-05-08 16:02:30 -0400

    need to change parameters

--- new.py
+++ new.py
@@ -1,54 +1,72 @@
-#UNFINISHED
 import numpy as np
 import pandas as pd
+
+transition_probabilities = {
+    'Healthy': {'Healthy': 0.95, 'Pre-diabetes': 0.04, 'Diabetes': 0.01},
+    'Pre-diabetes': {'Healthy': 0.1, 'Pre-diabetes': 0.85, 'Diabetes': 0.05},
+    'Diabetes': {'Healthy': 0, 'Pre-diabetes': 0, 'Diabetes': 1},
+}
+
+state_costs = {
+    'Healthy': 0,
+    'Pre-diabetes': 100,
+    'Diabetes': 500,
+}
 
 def generate_hypothetical_population(n):
     np.random.seed(42)
     population = []
     for _ in range(n):
         individual = {
-            'age': np.random.randint(18, 90), #change these distributions based on research data
+            'age': np.random.randint(18, 90),
             'BMI': np.random.uniform(18.5, 45),
             'gender': np.random.choice(['male', 'female']),
             'gestational_diabetes': np.random.choice([0, 1], p=[0.9, 0.1]),
             'family_history': np.random.choice([0, 1], p=[0.8, 0.2]),
             'race_ethnicity': np.random.choice(['low_risk', 'high_risk'], p=[0.7, 0.3]),
-            'physical_inactivity': np.random.choice([0, 1], p=[0.6, 0.4]),
-            'hypertension': np.random.choice([0, 1], p=[0.7, 0.3]),
-            'HDL': np.random.uniform(20, 100),
-            'TG': np.random.uniform(50, 400),
-            'glucose_tolerance': np.random.choice([0, 1], p=[0.85, 0.15]),
-            'PCOS': np.random.choice([0, 1], p=[0.9, 0.1]),
-            'history_of_CVD': np.random.choice([0, 1], p=[0.9, 0.1]),
-            'baby_weight_risk': np.random.choice([0, 1], p=[0.9, 0.1]),
-            'antipsychotic_therapy': np.random.choice([0, 1], p=[0.95, 0.05]),
-            'sleep_disorder_risk': np.random.choice([0, 1], p=[0.9, 0.1]),
-            'last_screened': -999
+            'physical_inactivity': int(np.random.choice([0, 1], p=[0.6, 0.4])),
+            'hypertension': int(np.random.choice([0, 1], p=[0.7, 0.3])),
+            'glucose_tolerance': int(np.random.choice([0, 1], p=[0.85, 0.15])),
+            'PCOS': int(np.random.choice([0, 1], p=[0.9, 0.1])),
+            'history_of_CVD': int(np.random.choice([0, 1], p=[0.9, 0.1])),
+            'baby_weight_risk': int(np.random.choice([0, 1], p=[0.9, 0.1])),
+            'antipsychotic_therapy': int(np.random.choice([0, 1], p=[0.95, 0.05])),
+            'sleep_disorder_risk': int(np.random.choice([0, 1], p=[0.9, 0.1])),
+            'health_state': np.random.choice(['Healthy', 'Pre-diabetes', 'Diabetes'], p=[0.7, 0.25, 0.05]),
+            'last_screened': -999,
+            'TG': np.random.randint(50, 300),
+            'HDL': np.random.randint(30, 100) # Add HDL key with random integer value
         }
         population.append(individual)
     return population
 
 
-def is_eligible_for_screening(individual, recommendation):
+
+def is_eligible_for_screening(individual, recommendation, current_year):
     age = individual['age']
     BMI = individual['BMI']
     gender = individual['gender']
     race_ethnicity = individual['race_ethnicity']
     last_screened = individual['last_screened']
+    screening_interval = 3
+
+    if current_year - last_screened < screening_interval:
+        return False
 
     risk_factors = {
         'physical_inactivity': individual['physical_inactivity'],
         'hypertension': individual['hypertension'],
-        'hdl_risk': individual['HDL'] < 35,
-        'tg_risk': individual['TG'] > 250,
-        'family_history': individual['family_history'],
+        'hdl_risk': int(individual['HDL'] < 40),
+        'TG_risk': int(individual['TG'] >= 150),
+        'glucose_tolerance': individual['glucose_tolerance'],
         'gestational_diabetes': individual['gestational_diabetes'],
-        'glucose_tolerance_risk': individual['glucose_tolerance'],
-        'pcos': individual['PCOS'],
-        'history_of_cvd': individual['history_of_CVD'],
+        'family_history': individual['family_history'],
+        'race_ethnicity': 1 if individual['race_ethnicity'] == 'high_risk' else 0,
+        'PCOS': individual['PCOS'],
+        'history_of_CVD': individual['history_of_CVD'],
         'baby_weight_risk': individual['baby_weight_risk'],
         'antipsychotic_therapy': individual['antipsychotic_therapy'],
-        'sleep_disorder_risk': individual['sleep_disorder_risk']
+        'sleep_disorder_risk': individual['sleep_disorder_risk'],
     }
 
     risk_factor_count = sum(risk_factors.values())
@@ -62,32 +80,49 @@ def is_eligible_for_screening(individual, recommendation):
     else:
         return False
 
-def apply_screening(population, recommendation, test_cost):
-    cost = 0
-    num_screened = 0
-    for individual in population:
-        if is_eligible_for_screening(individual, recommendation):
-            cost += test_cost[recommendation]
-            num_screened += 1
-            individual['screened'] = 1
-    return cost, num_screened
 
-# Change these cost values according to your research data
-test_cost = {
-    'USPSTF': 50,  # Adjust cost for USPSTF screening
-    'ADA': 50,  # Adjust cost for ADA screening
-    'AACE': 50  # Adjust cost for AACE screening
-}
+def select_screening_guideline(individual):
+    eligible_guidelines = []
+    if is_eligible_for_screening(individual, "USPSTF", current_year):
+        eligible_guidelines.append("USPSTF")
+    if is_eligible_for_screening(individual, "ADA", current_year):
+        eligible_guidelines.append("ADA")
+    if is_eligible_for_screening(individual, "AACE", current_year):
+        eligible_guidelines.append("AACE")
+    if len(eligible_guidelines) > 0:
+        selected_guideline = np.random.choice(eligible_guidelines)
+    else:
+        selected_guideline = None
+    return selected_guideline
 
-# Generate a hypothetical population of 1000 individuals
-population_size = 1000  # You can adjust the population size
-population = generate_hypothetical_population(population_size)
+def screen_individual(individual, guideline):
+    health_state = individual['health_state']
+    if guideline == "USPSTF" and health_state in ["Pre-diabetes", "Diabetes"]:
+        health_state = "Healthy"
+    elif guideline == "ADA" and health_state == "Diabetes":
+        health_state = "Pre-diabetes"
+    elif guideline == "AACE" and health_state == "Pre-diabetes":
+        health_state = "Diabetes"
+    individual['health_state'] = health_state
+    individual['last_screened'] = current_year
+    return individual
 
-# Apply the screening for each recommendation and calculate the cost
-uspstf_cost, uspstf_screened = apply_screening(population, "USPSTF", test_cost)
-ada_cost, ada_screened = apply_screening(population, "ADA", test_cost)
-aace_cost, aace_screened = apply_screening(population, "AACE", test_cost)
+def run_simulation(population, num_years):
+    results = []
+    for year in range(num_years):
+        global current_year
+        current_year = year
+        screened_counts = {'USPSTF': 0, 'ADA': 0, 'AACE': 0}
+        for individual in population:
+            guideline = select_screening_guideline(individual)
+            if guideline is not None:
+                screened_counts[guideline] += 1
+                individual = screen_individual(individual, guideline)
+        results.append(screened_counts)
+    return results
 
-print("USPSTF cost:", uspstf_cost, "Number of people screened:", uspstf_screened)
-print("ADA cost:", ada_cost, "Number of people screened:", ada_screened)
-print("AACE cost:", aace_cost, "Number of people screened:", aace_screened)
+# 10 years:
+population = generate_hypothetical_population(1000)
+results = run_simulation(population, 10)
+df_results = pd.DataFrame(results)
+print(df_results)
